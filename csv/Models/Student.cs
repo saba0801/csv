6c0a995bec242da0a810d56499fd08637bd3e973
saba0 <saba0@DESKTOP-IE2B09D>
2020-02-04 14:31:30 -0500

    Add project files.

---
+++ csv/Models/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csv.Models
+{
+   public class Student
+    {
+        public string StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+       
+        
+        private string _DateOfBirth;
+
+        public string DateOfBirth
+        {
+            get { return _DateOfBirth; }
+            set
+            {
+                _DateOfBirth = value;
+                //convert Dateof Birth to DateTime
+                DateTime dtOut;
+                DateTime.TryParse(_DateOfBirth, out dtOut);
+                DateOfBirthDT = dtOut;
+            }
+        }
+            public DateTime DateOfBirthDT { get; internal set; }
+        public string ImageData { get; set; }
+        public void FromCSV(string csvdata)
+        {
+            string[] data = csvdata.Split(",", StringSplitOptions.None);
+            StudentId = data[0];
+            FirstName = data[1];
+            LastName = data[2];
+            DateOfBirth = data[3];
+            ImageData = data[4];
+        }
+        public string ToCSV()
+        {
+            string result = $"{StudentId},{FirstName},{LastName},{DateOfBirthDT.ToShortDateString()},{ImageData}";
+            return result;
+        }
+        public override string ToString()
+        {
+            string result = $"{StudentId} {FirstName} {LastName}";
+            return result;
+            
+        }
+
+    }
+}
---
+++ csv/Program.cs
@@ -0,0 +1,41 @@
+﻿using csv.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace csv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\saba0\Desktop\Big data\1st Sem\Information encoding standard\info.csv";
+            string fileContents;
+            using (StreamReader stream = new StreamReader(filePath))
+            {
+                fileContents = stream.ReadToEnd();
+            }
+            List<string> entries = new List<string>();
+            entries = fileContents.Split("\r\n", StringSplitOptions.RemoveEmptyEntries).ToList();
+            // string[] data = entries[1].Split(",", StringSplitOptions.None);
+            Student student = new Student();
+            student.FromCSV(entries[1]);
+
+            // Student student = new Student
+            // {
+            //   StudentId = data[0],
+            // FirstName = data[1],
+            //LastName = data[2],
+            //DateOfBirth = data[3],
+            //ImageData = data[4]
+            //};
+
+            Console.WriteLine(student.ToCSV());
+            Console.WriteLine(student.ToString());
+            Console.WriteLine(fileContents);
+        
+            
+        }
+    }
+}
